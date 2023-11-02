bb07a2b1c084d53607fe29785e5b7953fe645189
Afolabi Opeyemi <yemijoshua81@gmail.com>
2023-11-02 14:02:32 +0100

    adding mission and vision, to home page

--- client/src/components/main/Main.js
+++ client/src/components/main/Main.js
@@ -3,7 +3,7 @@ import img1 from "../../assets/DAP/img1.jpg";
 import img2 from "../../assets/DAP/img2.jpg";
 import img3 from "../../assets/DAP/img3.jpg";
 import img4 from "../../assets/DAP/img4.jpg";
-import img5 from "../../assets/DAP/img5.jpg";
+import img6 from "../../assets/DAP/img6.jpeg";
 import "./main.css";
 
 const Main = () => {
@@ -25,6 +25,9 @@ const Main = () => {
               <img src={img3} />
             </div>
             <div className="item">
+              <img src={img6} />
+            </div>
+            <div className="item">
               <img src={img4} />
             </div>
           </div>
@@ -32,17 +35,25 @@ const Main = () => {
       </main>
 
       <section className="parallax">
-        <div><h1>Help us to care for you!</h1></div>
+        <div>
+          <h1>Help us to care for you!</h1>
+        </div>
       </section>
 
       <section className="mission-statement">
         <div className="vision">
           <h3>Vision</h3>
-          <p>lorem do you ipsium do lor random text</p>
+          <p>
+            To create home away from home for our teaming indigens across the
+            world.
+          </p>
         </div>
         <div className="mission">
           <h3>Mission</h3>
-          <p>lorem do you ipsium do lor random text</p>
+          <p>
+            To build relationship with our diaspora for the purpose of fostering
+            economic relations and enhancing knowledge transfer.
+          </p>
         </div>
       </section>
     </section>
